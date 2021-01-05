c46d6ca6fd1d246012852f35130709e2e1ee6e51
DenisVASI <denisvasilenkoise@outlook.com>
2021-01-05 11:27:12 +0500

    first commit

---
+++ App.tsx
@@ -0,0 +1,65 @@
+import React, {useEffect} from 'react';
+import {PermissionsAndroid, Platform, Text} from 'react-native';
+// @ts-ignore
+import {launchImageLibrary} from 'react-native-image-picker';
+
+const testPermissions = async () => {
+  if (Platform.OS === 'android') {
+    const permission = PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE;
+    const hasPermission = await PermissionsAndroid.check(permission);
+    console.log('@has', hasPermission);
+    if (!hasPermission) {
+      const status = await PermissionsAndroid.request(permission);
+      console.log('@status', status);
+    }
+  }
+};
+
+const fetchCircles = async (data: FormData) => {
+  await fetch('https://ai-tonometry.com/api/v1/circles/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'multipart/form-data; charset=utf-8',
+    },
+    body: data,
+  })
+    .then((res) => res.json())
+    .then((res) => console.log(res));
+};
+
+interface PickerPhoto {
+  uri: string;
+  fileName: string;
+  type: string;
+}
+
+const App = () => {
+  useEffect(() => {
+    testPermissions().then(async () => {
+      launchImageLibrary(
+        {
+          mediaType: 'photo',
+          storageOptions: {
+            skipBackup: true,
+          },
+        },
+        async (photo: PickerPhoto) => {
+          const data = new FormData();
+          data.append('image', {
+            name: photo.fileName,
+            type: photo.type,
+            uri:
+              Platform.OS === 'android'
+                ? photo.uri
+                : photo.uri.replace('file://', ''),
+          });
+          data.append('image', 'true');
+          await fetchCircles(data);
+        },
+      );
+    });
+  }, []);
+  return <Text> hello world</Text>;
+};
+
+export default App;
